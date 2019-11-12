7baf4a1645595281e9fd648dbad8c29d7ba71de0
Sang Pham <sang.pham@metanet.co.kr>
2019-11-12 15:38:40 +0700

    init

--- src/main/java/com/task/Shared/model/Book.java
+++ src/main/java/com/task/Shared/model/Book.java
@@ -1,4 +1,4 @@
-package com.task.server.domain;
+package com.task.Shared.domain;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
--- src/main/java/com/task/client/event/DescWindowEvents/CancelDescEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.task.client.event.DescWindowEvents;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface CancelDescEventHandler extends EventHandler {
-    void onCancelDesc(CancelDescEvent event);
-}
